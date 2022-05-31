4ca4adb1fde62833d64557eb47c79a8b9f46f8d4
Jacob <JacobJardineuni@gmail.com>
2022-05-31 15:41:46 +0100

    Implemented CreateStatement Method
    Implemented CreateStatement Method within StatementService

--- src/com/jacob/codingexercise/Main.java
+++ src/com/jacob/codingexercise/Main.java
@@ -1,9 +1,23 @@
 package com.jacob.codingexercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jacob.codingexercise.model.Transaction;
+import com.jacob.codingexercise.service.IStatementService;
+import com.jacob.codingexercise.service.StatementService;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		List<Transaction> transactionList = new ArrayList<Transaction>();
+		IStatementService statement = new StatementService(transactionList);
+		if(statement.CreateTransactions(10) == true) {
+			System.out.println(true);
+		}else {
+			System.out.println(false);
+		}
 	}
 
 }
--- src/com/jacob/codingexercise/service/StatementService.java
+++ src/com/jacob/codingexercise/service/StatementService.java
@@ -1,15 +1,72 @@
 package com.jacob.codingexercise.service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 
+import com.jacob.codingexercise.enums.Category;
+import com.jacob.codingexercise.enums.Type;
 import com.jacob.codingexercise.model.Transaction;
 
 public class StatementService implements IStatementService{
 
+	private List<Transaction> _transaction;
+	
+	public StatementService(List<Transaction> _transaction) {
+		this._transaction = _transaction;
+	}
+	
 	@Override
 	public Boolean CreateTransactions(int numOfTransactions) {
-		// TODO Auto-generated method stub
-		return null;
+		if(numOfTransactions == 0) {
+			return false;
+		}
+		
+		try {
+			// Arrays that contain vendor names associated to categories
+			String[] ddArr = {"CYBG", "PureGym"};
+			String[] groceriesArr = {"Asda", "Morrisons", "M&S"};
+			String[] otherArr = {"McMillan"};
+			
+			double Amount = 0.0;
+			Category Category = null;
+			LocalDate TransactionDate;
+			Type Type = null;
+			String Vendor = "";
+			
+			for(int i = 0; i < numOfTransactions; i++) {
+				switch(RandomNumber(3)) {
+					case 0:
+						Amount = (double) Math.round(RandomNumber(999)/10.0);
+						Category = Category.DD;
+						Type = Type.DD;
+						Vendor = ddArr[RandomNumber(ddArr.length)].toString();
+						break;
+					case 1:
+						Amount = (double) Math.round(RandomNumber(999)/10.0);
+						Category = Category.GROCERIES;
+						Type = Type.CARD;
+						Vendor = groceriesArr[RandomNumber(groceriesArr.length)].toString();
+						break;
+					case 2:
+						Amount = (double) Math.round(RandomNumber(999)/10.0);
+						Category = Category.OTHER;
+						Type = Type.INTERNET;
+						Vendor = otherArr[RandomNumber(otherArr.length)].toString();
+						break;
+				}
+				
+				TransactionDate = TransactionDate(365);
+				
+				_transaction.add(new Transaction(Amount, Category, TransactionDate, Type, Vendor));
+				
+				return true;
+			}
+			
+		}catch(Exception e) {
+			return false;
+		}
+		return false;
 	}
 
 	@Override
@@ -22,6 +79,16 @@ public class StatementService implements IStatementService{
 	public List<Transaction> GroubByCategory() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private int RandomNumber(int range) {
+		Random randomNumber = new Random();
+		return randomNumber.nextInt(range);
+	}
+	
+	private LocalDate TransactionDate(int days) {
+		LocalDate date = LocalDate.now();
+		return date.minusDays(days);
 	}
 
 }
